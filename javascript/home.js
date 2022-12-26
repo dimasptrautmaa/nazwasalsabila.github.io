88ed857865c7af33ee8c6e245640bb0b8052ea89
dimasptrautmaa <dimasptraautmaa@gmail.com>
2022-12-26 19:39:06 +0700

    update

--- javascript/home.js
+++ javascript/home.js
@@ -7,13 +7,13 @@ const info = document.getElementById('info');
 const ulimg1 = document.getElementById('ul-img1');
 const ulimg2 = document.getElementById('ul-img2');
 const wrapper = document.getElementById('wrapper');
+const box = document.getElementById('box');
 
 playbtn.addEventListener('click', function(){
     vn.play();
     after.style.display = 'flex';
     before.style.display = 'none';
     wrapper.style.filter = 'none';
-    box.classList.add('grey');
 })
 
 ulimg1.addEventListener('click', function(){
