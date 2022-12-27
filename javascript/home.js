643c8058f0af3d883e2091c4678ee6360907162f
dimasptrautmaa <dimasptraautmaa@gmail.com>
2022-12-27 10:16:18 +0700

    update

--- javascript/home.js
+++ javascript/home.js
@@ -8,12 +8,16 @@ const ulimg1 = document.getElementById('ul-img1');
 const ulimg2 = document.getElementById('ul-img2');
 const wrapper = document.getElementById('wrapper');
 const box = document.getElementById('box');
+const boxleft = document.getElementById('box-left')
+const boxleft2 = document.getElementById('box-left-2')
 
 playbtn.addEventListener('click', function(){
     vn.play();
     after.style.display = 'flex';
     before.style.display = 'none';
     wrapper.style.filter = 'none';
+    boxleft.style.display = 'none';
+    boxleft2.style.display = 'flex';
 })
 
 ulimg1.addEventListener('click', function(){
